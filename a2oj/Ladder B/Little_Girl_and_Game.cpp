b20c2ecabbd8d54c3fe5917bb355a24b4c65ac8c
cipherLord <sarthakjohnsonprasad@gmail.com>
2020-06-24 22:01:45 +0530

    24 Jun 2020

--- a2oj/Ladder B/Little_Girl_and_Game.cpp
+++ a2oj/Ladder B/Little_Girl_and_Game.cpp
@@ -6,14 +6,11 @@
 #define LOOP(i,k,n) for (ll i = k; i <= n; ++i) 
 #define LOOPREV(i,k,n) for (ll i = k; i >= n; --i) 
 #define mp make_pair(x,y)
-#define pa pair <ll,ll>
-#define vec vector<ll>
-#define vec2D vector <ll<ll>>
-#define iterate vector<ll>::iterator
+#define pa pair <int,int>
+#define vec vector<int>
+#define vec2D vector <int<int>>
 #define ll long long int
 #define ld long double
-#define pub push_back
-#define pob pop_back
 
 using namespace std;
 
@@ -22,4 +19,23 @@ int main()
     fastio;
     cin.tie(NULL);
     cout.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    vec v(26,0);
+    loop(i,s.length()){
+        v[(int)(s[i] - 'a')]++;
+    }
+
+    int count = 0;
+
+    loop(i,26){
+        if(v[i]%2 == 1) count++;
+    }
+
+    if(count==0 || count%2==1) cout << "First";
+    else cout << "Second";
+
 }
+
--- a2oj/Ladder B/Petya_and_Staircases.cpp
+++ a2oj/Ladder B/Petya_and_Staircases.cpp
@@ -17,9 +17,38 @@
 
 using namespace std;
 
+
 int main()
 {
     fastio;
     cin.tie(NULL);
     cout.tie(NULL);
+
+    ll n,m;
+    cin >> n >> m;
+
+    vec v;
+    v.resize(m);
+    ll temp;
+
+    loop(i,m){
+        cin >> temp;
+        if(temp==1 || temp == n){
+            cout << "NO";
+            return 0;
+        }
+        v.pub(temp);
+    }
+
+    sort(v.begin(),v.end());
+    iterate it;
+
+    for(it = v.begin(); it < v.end(); ++it){
+        if(*it +1 == *(it+1) && *(it+1) +1 == *(it+2)){
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+    return 0;
 }
